fc7b1b8bb0d12917c03c98ca48ac1ec437a5a1fa
konst-teknik <information-konst-teknik.se>
2023-03-02 14:32:26 +0100

    Footer

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -1,6 +1,8 @@
 import s from './Footer.module.scss'
 import cn from 'classnames'
 import type { MenuItem } from '/lib/menu'
+import KFLogo from '/public/images/kf-logo.svg'
+
 
 export type FooterProps = {
 	menu: MenuItem[],
@@ -9,7 +11,23 @@ export type FooterProps = {
 export default function Footer({ menu }: FooterProps) {
 	return (
 		<footer className={cn(s.footer)} id="footer">
-			Footer
+			<section>
+				<div>
+					Copyright Luleåbiennalen, 2023 <br />
+					info@luleabiennalen.se
+					Cookies & GDPR
+				</div>
+				<div>
+					Prenumerera på vårt nyhetsbrev <br />
+					Följ oss på:     Facebook     Instagram
+				</div>
+				<div>
+					Luleåbiennalen är ett projekt inom organisationen Konstfrämjandet Norrbotten sedan 2021.
+				</div>
+				<KFLogo className={s.kf} />
+
+			</section>
+
 		</footer>
 	)
 }
