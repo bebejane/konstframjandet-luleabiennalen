8b366747d01a649f054aa2595e5555d1a45a702f
Bebe Jane <bjornthief@gmail.com>
2023-03-03 12:58:01 +0100

    fade

--- components/layout/Article.tsx
+++ components/layout/Article.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 import { StructuredContent } from "/components";
 import { Image } from 'react-datocms/image';
 import { useScrollInfo } from 'dato-nextjs-utils/hooks'
-
 import format from 'date-fns/format';
 
 export type ArticleProps = {
@@ -21,20 +20,28 @@ export type ArticleProps = {
   date?: string
 }
 
+const MAX_PORTRAIT_HEIGHT = 600;
+
 export default function Article({ id, children, title, content, image, imageSize, intro, date, onClick, record }: ArticleProps) {
 
   const { scrolledPosition, viewportHeight } = useScrollInfo()
   const ratio = Math.min(1, scrolledPosition / viewportHeight);
   const padding = `${ratio * 100}px`;
-  //console.log(ratio)
+  const opacity = Math.max(0, 1 - (ratio * 4));
+  const isPortrait = image.height > image.width
+  const portraitStyle = {}
 
   return (
     <div className={cn(s.article, 'article')}>
       <h1>{title}</h1>
       {image &&
         <figure className={cn(s.mainImage, imageSize && s[imageSize])}>
-          <Image data={image.responsiveImage} pictureStyle={{ padding }} />
-          <figcaption>{image.title}</figcaption>
+          <Image
+            data={image.responsiveImage}
+            pictureClassName={s.picture}
+            pictureStyle={{ padding, ...portraitStyle }}
+          />
+          <figcaption style={{ opacity }}>{image.title}</figcaption>
         </figure>
       }
       <section className="intro">
