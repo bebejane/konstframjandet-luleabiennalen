e996112a16ec1f68389a2ebfb3e012e737c5f8c9
Bebe Jane <bjornthief@gmail.com>
2023-03-07 18:48:20 +0100

    hotdog

--- components/layout/Hamburger.tsx
+++ components/layout/Hamburger.tsx
@@ -1,23 +1,24 @@
 import s from './Hamburger.module.scss'
 import cn from 'classnames'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import useStore from '/lib/store'
 
 export default function Hamburger() {
 
   const [showMenu, setShowMenu] = useStore((state) => [state.showMenu, state.setShowMenu])
-
+  const [key, setKey] = useState(Math.random())
   const handleClick = (e) => {
     setShowMenu(!showMenu)
+    setKey(Math.random())
     e.stopPropagation();
   }
 
   return (
     <div className={s.hamburger} onClick={handleClick}>
       <div className={s.wrap}>
-        <div className={cn(showMenu ? s.open : s.close)}></div>
-        <div className={cn(showMenu ? s.open : s.close)}></div>
-        <div className={cn(showMenu ? s.open : s.close)}></div>
+        <div id="l1" key={`${key}-1`} className={cn(!showMenu ? s.opened : s.closed)}></div>
+        <div id="l2" key={`${key}-2`} className={cn(!showMenu ? s.opened : s.closed)}></div>
+        <div id="l3" key={`${key}-3`} className={cn(!showMenu ? s.opened : s.closed)}></div>
       </div>
     </div>
   )
