d07995f1ea4bd8f99aba935441078c972db53566
Bebe Jane <bjornthief@gmail.com>
2023-03-08 23:32:10 +0100

    revalidate

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,21 +1,42 @@
+import years from '/lib/years.json'
 import { withRevalidate } from 'dato-nextjs-utils/hoc'
 
 export default withRevalidate(async (record, revalidate) => {
 
   const { api_key: apiKey, } = record.model;
   const { slug } = record
+  const year = years.find(({ id }) => record.year === id)
+  const prefix = !year || year?.title === process.env.NEXT_PUBLIC_CURRENT_YEAR ? '' : `/${year.title}`
 
   const paths = []
 
   switch (apiKey) {
     case 'about':
       paths.push(`/om/${slug}`)
+      prefix && paths.push(`${prefix}/om/${slug}`)
       break;
-    case 'project':
-      paths.push(`/projekt/${slug}`)
+    case 'program':
+      paths.push(`/program/${slug}`)
+      prefix && paths.push(`${prefix}/program/${slug}`)
+      break;
+    case 'participant':
+      paths.push(`/medverkande/${slug}`)
+      prefix && paths.push(`${prefix}/medverkande/${slug}`)
       break;
     case 'news':
       paths.push(`/nyheter/${slug}`)
+      prefix && paths.push(`${prefix}/nyheter/${slug}`)
+      break;
+    case 'locations':
+      paths.push(`/platser/${slug}`)
+      prefix && paths.push(`${prefix}/platser/${slug}`)
+      break;
+    case 'exhibitions':
+      paths.push(`/utstallningar/${slug}`)
+      prefix && paths.push(`${prefix}/utstallningar/${slug}`)
+      break;
+    case 'contact':
+      paths.push(`/kontakt`)
       break;
     default:
       break;
