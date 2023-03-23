3a7e5faa784123a05c2143052ab33631a43b7cd4
Bebe Jane <bjornthief@gmail.com>
2023-03-23 15:21:04 +0100

    dato link title

--- components/nav/DatoLink.tsx
+++ components/nav/DatoLink.tsx
@@ -10,7 +10,7 @@ export default function DatoLink({ link, className }: Props) {
   if (!link) return null
 
   const slug = link.__typename === 'ExternalLinkRecord' ? link.url : recordToSlug(link.record)
-  const title = link.__typename === 'ExternalLinkRecord' ? link.title : link.record.__typename === 'ParticipantRecord' ? link.record.name : link.record.title
+  const title = link.__typename === 'ExternalLinkRecord' ? link.title : link.title ?? (link.record.__typename === 'ParticipantRecord' ? link.record.name : link.record.title)
 
   return <Link href={slug} className={className}>{title}</Link>
 
