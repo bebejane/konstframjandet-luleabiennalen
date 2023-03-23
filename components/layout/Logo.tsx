70ef741c083dc4c474a0730c4b8813c37a155ef1
Bebe Jane <bjornthief@gmail.com>
2023-03-23 15:30:16 +0100

    logo home display node

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -9,6 +9,7 @@ export type Props = {
 
 export default function Logo({ }: Props) {
   const { year: { color: { hex } } } = usePage()
+
   return (
     <div className={s.container} style={{ fill: hex }}>
       <Link href={'/'}><LogoIcon /></Link>
