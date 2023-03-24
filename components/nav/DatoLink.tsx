646e9a446d86e5dbd4fde82961b5a63a61880fc3
Bebe Jane <bjornthief@gmail.com>
2023-03-24 10:14:47 +0100

    fix DatoLink external site

--- components/nav/DatoLink.tsx
+++ components/nav/DatoLink.tsx
@@ -13,6 +13,11 @@ export default function DatoLink({ link, className, children }: Props) {
   const slug = link.__typename === 'ExternalLinkRecord' ? link.url : recordToSlug(link.record)
   const title = link.__typename === 'ExternalLinkRecord' ? link.title : link.internalTitle || (link.record.__typename === 'ParticipantRecord' ? link.record.name : link.record.title)
 
-  return <Link href={slug} className={className}>{children ?? title}</Link>
+  return (
+    link.__typename === 'ExternalLinkRecord' ?
+      <a href={slug}>{children ?? title}</a>
+      :
+      <Link href={slug} className={className}>{children ?? title}</Link>
+  )
 
 }
