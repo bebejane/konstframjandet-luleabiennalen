9af11a2a039f08244e0bf8c41f9de9dd831ddbd7
Bebe Jane <bjornthief@gmail.com>
2023-03-24 18:00:42 +0100

    fullbredd videeo

--- components/blocks/StartFullscreenVideo.tsx
+++ components/blocks/StartFullscreenVideo.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 import { VideoPlayer, DatoLink } from '/components'
 import Link from 'next/link'
 import { useRef } from 'react'
+import { DatoMarkdown as Markdown } from 'dato-nextjs-utils/components'
 
 export type Props = { data: StartFullscreenVideoRecord }
 
-export default function StartFullscreenVideo({ data: { video, text, link } }: Props) {
+export default function StartFullscreenVideo({ data: { video, text, headline, link } }: Props) {
 
 	const ref = useRef()
 
@@ -16,18 +17,8 @@ export default function StartFullscreenVideo({ data: { video, text, link } }: Pr
 				<VideoPlayer data={video} />
 			</DatoLink>
 			<div className={s.textWrap}>
-				<div className={s.text}>
-					<div><h1 className="start">{text}</h1></div>
-					<div className={s.link}>
-						<DatoLink link={link}>
-							<a>
-								<span className="medium white">
-									link
-								</span>
-							</a>
-						</DatoLink>
-					</div>
-				</div>
+				<h2>{headline}</h2>
+				<Markdown className={s.text}>{text}</Markdown>
 			</div>
 		</section>
 	)
