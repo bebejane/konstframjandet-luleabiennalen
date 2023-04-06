4e1334bb9412fc371d6c672efe7bb2c9210ce653
Bebe Jane <bjornthief@gmail.com>
2023-04-06 10:52:21 +0200

    lang

--- components/nav/Language.tsx
+++ components/nav/Language.tsx
@@ -3,10 +3,17 @@ import cn from 'classnames'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { capitalize, pathToMenuItem, defaultLocale } from '/lib/utils'
+import { Menu } from '/lib/menu'
+
+export type Props = {
+	menu: Menu
+}
 
 export default function Language({ menu }) {
 	const { locale, locales, asPath } = useRouter()
-	//const menuItem = pathToMenuItem(asPath, locale, menu)
+	const menuItem = pathToMenuItem(asPath, locale, menu)
+	console.log(asPath)
+	console.log(menuItem)
 
 	return (
 		<nav className={s.language}>
