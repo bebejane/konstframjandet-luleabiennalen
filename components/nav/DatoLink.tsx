721eb10780cbfd44fd10466903860196e1ddf5c1
Bebe Jane <bjornthief@gmail.com>
2023-04-26 15:12:29 +0200

    fixes

--- components/nav/DatoLink.tsx
+++ components/nav/DatoLink.tsx
@@ -2,7 +2,7 @@ import Link from './Link'
 import { recordToSlug } from '/lib/utils'
 
 export type Props = {
-  link: ExternalLinkRecord | (InternalLinkRecord & { internalTitle: String }) | any
+  link: ExternalLinkRecord | InternalLinkRecord | any
   className?: string
   children?: React.ReactNode
 }
@@ -13,7 +13,7 @@ export default function DatoLink({ link, className, children }: Props) {
     return <a className={className}>{children}</a>
 
   const slug = link.__typename === 'ExternalLinkRecord' ? link.url : recordToSlug(link.record)
-  const title = link.__typename === 'ExternalLinkRecord' ? link.title : link.internalTitle || (link.record.__typename === 'ParticipantRecord' ? link.record.name : link.record.title)
+  const { title } = link
 
   return (
     link.__typename === 'ExternalLinkRecord' ?
