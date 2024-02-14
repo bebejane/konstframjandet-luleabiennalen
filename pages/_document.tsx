8525d3478b1bea39d405831b20fe3d365a8aad05
Bebe Jane <bjornthief@gmail.com>
2024-02-14 14:52:41 +0100

    ahhhhh

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
+
   render() {
     const { year }: { year: YearExtendedRecord } = this.props?.__NEXT_DATA__?.props?.pageProps || {};
 
