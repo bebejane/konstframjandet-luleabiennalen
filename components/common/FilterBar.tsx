de3d7bc3911ac36c39678be428987ab9959b0417
Bebe Jane <bjornthief@gmail.com>
2025-04-08 15:25:28 +0200

    fix filterbar

--- components/common/FilterBar.tsx
+++ components/common/FilterBar.tsx
@@ -4,85 +4,53 @@ import { useEffect, useState } from 'react';
 import { useTranslations } from 'next-intl';
 
 export type FilterOption = {
-  id: string;
-  label: string;
-  description: string;
+	id: string;
+	label: string;
+	description: string;
 };
 
 export type Props = {
-  options: FilterOption[];
-  multi?: boolean;
-  category: string;
-  onChange: (value: string[] | string) => void;
+	options: FilterOption[];
+	multi?: boolean;
+	category: string;
+	onChange: (value: string[] | string) => void;
 };
 
 export default function FilterBar({ options = [], onChange, multi = false, category }: Props) {
-  const t = useTranslations('FilterBar');
-  const [selected, setSelected] = useState<FilterOption[]>([]);
+	const t = useTranslations('FilterBar');
+	const [selected, setSelected] = useState<FilterOption[]>([]);
 
-  useEffect(() => {
-    onChange(multi ? selected.map(({ id }) => id) : selected[0]?.id);
-  }, [selected]);
+	useEffect(() => {
+		onChange(multi ? selected.map(({ id }) => id) : selected[0]?.id);
+	}, [selected]);
 
-  return (
-    <nav className={s.filter}>
-      <ul>
-        <li onClick={() => setSelected([])} className={cn(!selected?.length && s.selected)}>
-          {t('all')}&nbsp; {category}
-        </li>
-        {options.map((opt, idx) => (
-          <li
-            key={idx}
-            onClick={() =>
-              setSelected(
-                selected.find(({ id }) => id === opt.id)
-                  ? selected.filter(({ id }) => id !== opt.id)
-                  : multi
-                    ? [...selected, opt]
-                    : [opt]
-              )
-            }
-            className={cn(selected?.find(({ id }) => id === opt.id) && s.selected)}
-          >
-            {opt.label}
-          </li>
-        ))}
-      </ul>
-      {!multi && selected && <div className={s.description}>{selected[0]?.description}</div>}
-    </nav>
-  );
+	return (
+		<nav className={s.filter}>
+			<ul>
+				<li onClick={() => setSelected([])} className={cn(!selected?.length && s.selected)}>
+					{t('all')}&nbsp; {category}
+				</li>
+				{options.map((opt, idx) => (
+					<li
+						key={idx}
+						className={cn(selected?.find(({ id }) => id === opt.id) && s.selected)}
+						onClick={() =>
+							setSelected(
+								selected.find(({ id }) => id === opt.id)
+									? selected.filter(({ id }) => id !== opt.id)
+									: multi
+									? [...selected, opt]
+									: [opt]
+							)
+						}
+					>
+						{opt.label}
+					</li>
+				))}
+			</ul>
+			{!multi && selected && selected[0]?.description && (
+				<div className={s.description}>{selected[0]?.description}</div>
+			)}
+		</nav>
+	);
 }
-
-export default function FilterBar({ options = [], onChange, multi = false }: Props) {
-
-  const t = useTranslations('FilterBar')
-  const [selected, setSelected] = useState<FilterOption[]>([])
-
-  useEffect(() => {
-    onChange(multi ? selected.map(({ id }) => id) : selected[0]?.id)
-  }, [selected])
-
-  return (
-    <nav className={s.filter}>
-      <ul>
-        <li
-          onClick={() => setSelected([])}
-          className={cn(!selected?.length && s.selected)}
-        >{t('all')}</li>
-        {options.map((opt, idx) =>
-          <li
-            key={idx}
-            onClick={() => setSelected(selected.find(({ id }) => id === opt.id) ? selected.filter(({ id }) => id !== opt.id) : multi ? [...selected, opt] : [opt])}
-            className={cn(selected?.find(({ id }) => id === opt.id) && s.selected)}
-          >
-            {opt.label}
-          </li>
-        )}
-      </ul>
-      {(!multi && selected && selected[0]?.description) &&
-        <div className={s.description}>{selected[0].description}</div>
-      }
-    </nav>
-  )
-}
-
