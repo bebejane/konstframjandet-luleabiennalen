8fe5ba579dcdb1be27bc8a01a35a3c350e714aa7
Bebe Jane <bjornthief@gmail.com>
2026-09-11 09:39:30 +0200

    fix

--- datocms.config.ts
+++ datocms.config.ts
@@ -13,7 +13,9 @@ import years from '@/years.json';
 export function getRoute(item: any, locale?: string | null): string {
 	const apiKey = getItemApiKey(item);
 	if (!apiKey) throw new Error('No api key found');
-	const slug = typeof item.slug === 'string' ? item.slug : item.slug[locale ?? defaultLocale];
+
+	const slug =
+		typeof item.slug === 'string' ? item.slug : (item.slug?.[locale ?? defaultLocale] ?? null);
 	let route: string | null = null;
 
 	switch (apiKey) {
